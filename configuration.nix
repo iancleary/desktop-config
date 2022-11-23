8e85c2b31703d7f15008f4eb7f8c6c0292a875f6
iancleary <hello@iancleary.me>
2022-11-23 13:45:32 -0700

    Resolve vscode with extensions and vscode

--- configuration.nix
+++ configuration.nix
@@ -107,7 +107,6 @@
     yarn
     yadm
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    vscode
     (vscode-with-extensions.override {
       vscodeExtensions = with vscode-extensions; [
         bbenoist.nix
