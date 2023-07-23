cde915238177f357f06535a0ac2803dcddf8e8fa
iancleary <github@iancleary.me>
2023-07-23 12:04:21 -0700

    Add notes to vscode-fhs unstable channel nix file

--- modules/desktop/vscode.nix
+++ modules/desktop/vscode.nix
@@ -4,6 +4,10 @@
 
 { config, pkgs, ... }:
 let
+  # Passing config into the variable definition is necessary to allow
+  # the use of config.allowUnfree in the unstable channel.
+  # https://nixos.wiki/index.php?title=FAQ&oldid=3528#How_can_I_install_a_package_from_unstable_while_remaining_on_the_stable_channel.3F
+  # https://discourse.nixos.org/t/allow-unfree-from-unstable/23218/2
   unstable = import <nixos-unstable> { config.allowUnfree = true;};
 in {
   environment.systemPackages = [ 
