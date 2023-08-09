822d9a5935279037d768c2041455a70c80b4bbbd
iancleary <github@iancleary.me>
2023-08-08 18:52:13 -0700

    Switch boot mount point on nixos-framework

--- hosts/nixos-framework.nix
+++ hosts/nixos-framework.nix
@@ -65,7 +65,7 @@
   boot.loader = {
     efi = {
       canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
+      efiSysMountPoint = "/boot"; # ← use the same mount point here.
     };
     grub = {
        efiSupport = true;
