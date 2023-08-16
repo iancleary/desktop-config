a8a582ce5b124e54e507db59bfccbe953764f0bb
iancleary <github@iancleary.me>
2023-08-16 15:28:40 -0700

    Linting

--- flake.nix
+++ flake.nix
@@ -28,88 +28,88 @@
       work_variables = {
         username = "icleary";
         userfullname = "Ian Cleary";
-        useremail = "ian.cleary@viasat.com"
-          };
+        useremail = "ian.cleary@viasat.com";
+      };
 
-        x64_system = "x86_64-linux";
+      x64_system = "x86_64-linux";
 
-        x64_base_args = {
-          inherit nixpkgs;
-          system = x64_system;
-          config = {
-            allowUnfree = true;
-          };
-        };
-
-        bare-metal-modules = [
-          ./modules/bare-metal/fwupd.nix
-          ./modules/bare-metal/sound.nix
-          ./modules/bare-metal/printing.nix
-        ];
-
-        virtualbox-guest-modules = [
-          ./modules/virtualbox/guest-enabled.nix
-        ];
-
-        common-modules = [
-          # Flakes and Direnv
-          ./modules/flakes.nix
-          ./modules/nix-direnv.nix
-
-          # Common
-          ./modules/common/docker.nix
-          ./modules/common/localBinInPath.nix
-          ./modules/common/packages.nix
-          ./modules/common/zsh.nix
-
-          ./modules/networkmanager.nix
-          ./modules/openssh.nix
-          ./modules/garbage-collection.nix
-
-          ./modules/unfree-allowed.nix
-
-          # Locale and Timezone
-          ./modules/localization/en_US.nix
-          ./modules/timezone/America-Phoenix.nix
-        ];
-
-        personal-modules = [
-          ./modules/tailscale.nix
-        ];
-
-        desktop-modules = [
-          # Desktop Specific
-          ./modules/desktop/flatpak.nix
-          ./modules/desktop/gnome.nix
-          ./modules/desktop/packages.nix
-          ./modules/desktop/vscode.nix
-          ./modules/desktop/x11-keymap.nix
-
-        ];
-
-        in
-        {
-        nixosConfigurations = {
-          framework = nixpkgs.lib.nixosSystem {
-            extraArgs = x64_base_args;
-            specialArgs = personal_variables;
-            modules = bare-metal-modules ++ common-modules ++ desktop-modules
-              ++ [
-              ./hardware-configuration.nix # hardware-configuration/framework.nix
-              ./configuration.nix # hosts/framework.nix
-              ./home/default.nix
-            ];
-          };
-          vm-icleary-nixos = nixpkgs.lib.nixosSystem {
-            extraArgs = x64_base_args;
-            specialArgs = work_variables;
-            modules = common-modules ++ virtualbox-guest-modules ++ desktop-modules
-              ++ [
-              ./hardware-configuration.nix # hardware-configuration/vm-icleary-nixos.nix
-              ./configuration.nix # hosts/vm-icleary-nixos.nix
-              ./home/default.nix
-            ];
-          };
+      x64_base_args = {
+        inherit nixpkgs;
+        system = x64_system;
+        config = {
+          allowUnfree = true;
         };
       };
-      }
+
+      bare-metal-modules = [
+        ./modules/bare-metal/fwupd.nix
+        ./modules/bare-metal/sound.nix
+        ./modules/bare-metal/printing.nix
+      ];
+
+      virtualbox-guest-modules = [
+        ./modules/virtualbox/guest-enabled.nix
+      ];
+
+      common-modules = [
+        # Flakes and Direnv
+        ./modules/flakes.nix
+        ./modules/nix-direnv.nix
+
+        # Common
+        ./modules/common/docker.nix
+        ./modules/common/localBinInPath.nix
+        ./modules/common/packages.nix
+        ./modules/common/zsh.nix
+
+        ./modules/networkmanager.nix
+        ./modules/openssh.nix
+        ./modules/garbage-collection.nix
+
+        ./modules/unfree-allowed.nix
+
+        # Locale and Timezone
+        ./modules/localization/en_US.nix
+        ./modules/timezone/America-Phoenix.nix
+      ];
+
+      personal-modules = [
+        ./modules/tailscale.nix
+      ];
+
+      desktop-modules = [
+        # Desktop Specific
+        ./modules/desktop/flatpak.nix
+        ./modules/desktop/gnome.nix
+        ./modules/desktop/packages.nix
+        ./modules/desktop/vscode.nix
+        ./modules/desktop/x11-keymap.nix
+
+      ];
+
+    in
+    {
+      nixosConfigurations = {
+        framework = nixpkgs.lib.nixosSystem {
+          extraArgs = x64_base_args;
+          specialArgs = personal_variables;
+          modules = bare-metal-modules ++ common-modules ++ desktop-modules
+            ++ [
+            ./hardware-configuration.nix # hardware-configuration/framework.nix
+            ./configuration.nix # hosts/framework.nix
+            ./home/default.nix
+          ];
+        };
+        vm-icleary-nixos = nixpkgs.lib.nixosSystem {
+          extraArgs = x64_base_args;
+          specialArgs = work_variables;
+          modules = common-modules ++ virtualbox-guest-modules ++ desktop-modules
+            ++ [
+            ./hardware-configuration.nix # hardware-configuration/vm-icleary-nixos.nix
+            ./configuration.nix # hosts/vm-icleary-nixos.nix
+            ./home/default.nix
+          ];
+        };
+      };
+    };
+}
