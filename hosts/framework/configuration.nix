2e43f1ce2a6ae5a623b6c1e045339a30f948c6f1
iancleary <github@iancleary.me>
2023-08-20 17:58:42 -0700

    Reorganize framework bare metal specific config into configuration.nix for host

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -8,10 +8,64 @@
   # Define your hostname.
   networking.hostName = "framework";
 
-
   # testing fwupd on framework 11th gen intel
   # https://knowledgebase.frame.work/en_us/framework-laptop-bios-releases-S1dMQt6F#Linux_BIOS
+  # https://nixos.wiki/wiki/Fwupd
+  services.fwupd.enable = true;
   services.fwupd.enableTestRemote = true;
+
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+    cpuFreqGovernor = lib.mkDefault "ondemand";
+  };
+
+  # Enable thermal data
+  services.thermald.enable = true;
+
+  # Enable fingerprint support
+  services.fprintd.enable = true;
+
+  # extra opengl packages for intel graphics
+  hardware.opengl.extraPackages = with pkgs; [
+    mesa_drivers
+    vaapiIntel
+    vaapiVdpau
+    libvdpau-va-gl
+    intel-media-driver
+  ];
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    # alsamixer: https://nixos.wiki/wiki/ALSA
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # https://nixos.wiki/wiki/Bluetooth#Enabling_A2DP_Sink
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.settings = {
+    General = {
+      Enable = "Source,Sink,Media,Socket";
+    };
+  };
 
   # Bootloader.
   #boot.loader.grub.enable = true;
