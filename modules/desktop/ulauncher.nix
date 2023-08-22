b4d83ad003798d85b6b83bab63d1263b5dd6c03c
iancleary <github@iancleary.me>
2023-08-21 20:54:02 -0700

    Add systemd service for ulauncher

--- modules/desktop/ulauncher.nix
+++ modules/desktop/ulauncher.nix
@@ -15,4 +15,18 @@ in
   environment.systemPackages = [
     pkgs-unstable.ulauncher
   ];
+
+  # Service to start
+  # copied from https://discourse.nixos.org/t/ulauncher-and-the-debugging-journey/13141/5?u=iancleary
+  # modified to use pkg-unstable instead of pkgs
+  systemd.user.services.ulauncher = {
+    enable = true;
+    description = "Start Ulauncher";
+    script = "${pkgs-unstable.ulauncher}/bin/ulauncher --hide-window";
+
+    documentation = [ "https://github.com/Ulauncher/Ulauncher/blob/f0905b9a9cabb342f9c29d0e9efd3ba4d0fa456e/contrib/systemd/ulauncher.service" ];
+    wantedBy = [ "graphical.target" "multi-user.target" ];
+    after = [ "display-manager.service" ];
+  };
+
 }
