2dd920423769c1e66623b080d80a4e2517524b7e
iancleary <github@iancleary.me>
2023-09-11 13:02:05 -0700

    Remove copy pasta leftovers

--- home/iancleary-hyprland.nix
+++ home/iancleary-hyprland.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ pkgs, ... }:
+{ config, inputs, pkgs, ... }:
 let
   flake-compat = builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
 
@@ -12,41 +12,26 @@ let
 in
 {
   imports = [
+    inputs.home-manager.nixosModules.home-manager
+    hyprland.homeManagerModules.default
+
   ];
 
+  wayland.windowManager.hyprland = {
+    enable = true;
+  };
 
-}
-
-  { config, inputs, pkgs, ... }:
-  let
-    flake-compat = builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
-
-    hyprland = (import flake-compat {
-      src = builtins.fetchTarball "https://github.com/hyprwm/Hyprland/archive/master.tar.gz";
-    }).defaultNix;
-  in
-  {
+  home-manager.users.iancleary = {
+    home.stateVersion = "23.05";
     imports = [
-      inputs.home-manager.nixosModules.home-manager
-      hyprland.homeManagerModules.default
-
+      ./common.nix
+      ./hyperland # folder
     ];
-
-    wayland.windowManager.hyprland = {
-      enable = true;
-    };
-
-    home-manager.users.iancleary = {
-      home.stateVersion = "23.05";
-      imports = [
-        ./common.nix
-        ./hyperland # folder
-      ];
-    };
-    # Define a user account. Don't forget to set a password with ‘passwd’.
-    users.users.iancleary = {
-      isNormalUser = true;
-      description = "iancleary";
-      extraGroups = [ "networkmanager" "wheel" "docker" "vboxsf" ];
-    };
-  }
+  };
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.iancleary = {
+    isNormalUser = true;
+    description = "iancleary";
+    extraGroups = [ "networkmanager" "wheel" "docker" "vboxsf" ];
+  };
+}
