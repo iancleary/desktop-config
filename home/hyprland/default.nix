b5d235cb156c1a3c4f8c3a9da4c391c199cec213
iancleary <github@iancleary.me>
2023-09-11 17:58:13 -0700

    Swap wallpaper and backgrounds, since backgrounds exists

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -10,9 +10,9 @@
   home.file.".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;
   home.file.".config/hypr/hyprpaper.conf".target = ".config/hypr/hyprpaper.conf";
 
-  home.file."Pictures/backgrounds/justinmaller-red-black-abstract.jpg".source = ./justinmaller-red-black-abstract.jpg;
-  home.file."Pictures/backgrounds/justinmaller-red-black-abstract.jpg".target = "Pictures/backgrounds/justinmaller-red-black-abstract.jpg";
+  home.file."Pictures/wallpaper/justinmaller-red-black-abstract.jpg".source = ./justinmaller-red-black-abstract.jpg;
+  home.file."Pictures/wallpaper/justinmaller-red-black-abstract.jpg".target = "Pictures/wallpaper/justinmaller-red-black-abstract.jpg";
 
-  home.file."Pictures/backgrounds/justinmaller-blue-black-abstract.jpg".source = ./justinmaller-blue-black-abstract.jpg;
-  home.file."Pictures/backgrounds/justinmaller-blue-black-abstract.jpg".target = "Pictures/backgrounds/justinmaller-blue-black-abstract.jpg";
+  home.file."Pictures/wallpaper/justinmaller-blue-black-abstract.jpg".source = ./justinmaller-blue-black-abstract.jpg;
+  home.file."Pictures/wallpaper/justinmaller-blue-black-abstract.jpg".target = "Pictures/wallpaper/justinmaller-blue-black-abstract.jpg";
 }
