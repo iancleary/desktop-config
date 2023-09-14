10c5780b345747b380b6709bb07e132382edabb8
iancleary <github@iancleary.me>
2023-09-14 14:18:34 -0700

    Remove blur from hyprland.conf after warning

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -64,7 +64,7 @@ in
     enable = true;
     settings = rec {
       initial_session = {
-        command = "${pkgs.hyprland}/bin/Hyprland --config /home/iancleary/.config/hypr/hyprland.conf";
+        command = "${pkgs.hyprland}/bin/Hyprland";
         user = "iancleary";
       };
       default_session = initial_session;
