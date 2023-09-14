e5d2b0aa5193bf9c8fd08a4c7f66a60530c15d1b
iancleary <github@iancleary.me>
2023-09-14 15:54:08 -0700

    Remove commented out leftovers

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -22,7 +22,4 @@
 
   home.file.".config/hypr/wofi.css".source = ./wofi.css;
   home.file.".config/hypr/wofi.css".target = ".config/hypr/wofi.css";
-
-  # home.file.".config/hypr/greetd.toml".source = ./greetd.toml;
-  # home.file.".config/hypr/greetd.toml".target = ".config/hypr/greetd.toml";
 }
