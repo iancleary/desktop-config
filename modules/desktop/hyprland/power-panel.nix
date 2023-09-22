bdb7638d1dd1ea896d5ba32429dcba73e3067ddd
iancleary <github@iancleary.me>
2023-09-21 20:54:40 -0700

    Add rust package template

--- modules/desktop/hyprland/power-panel.nix
+++ modules/desktop/hyprland/power-panel.nix
@@ -1,9 +1,5 @@
-{ pkgs
-, lib
-, rustPlatform
-, fetchFromGitHub
-,
-}:
+{ lib, fetchFromGitHub, rustPlatform }:
+
 rustPlatform.buildRustPackage rec {
   pname = "power-panel";
   version = "0.1.0";
@@ -12,26 +8,10 @@ rustPlatform.buildRustPackage rec {
     owner = "iancleary";
     repo = pname;
     rev = version;
-    sha256 = lib.fakeSha256;
+    hash = lib.fakeSha256;
   };
 
-  cargoSha256 = lib.fakeSha256;
-
-  # https://nixos.wiki/wiki/Packaging/Quirks_and_Caveats
-  nativeBuildInputs = with pkgs; [
-    pkg-config
-  ];
-
-  buildInputs = with pkgs; [
-    wayland
-    gtk4
-  ];
-
-  preFixup = ''
-    cat > $out/bin/power-panel <<EOF
-    EOF
-    chmod +x $out/bin/power-panel
-  '';
+  cargoHash = lib.fakeHash;
 
   meta = with lib; {
     description = "Rust GUI app to restart and shutdown a system";
