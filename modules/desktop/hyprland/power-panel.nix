cd45609e57dc850fb649d55129baefdbefbea355
iancleary <github@iancleary.me>
2023-09-21 21:10:26 -0700

    Package builds

--- modules/desktop/hyprland/power-panel.nix
+++ modules/desktop/hyprland/power-panel.nix
@@ -1,17 +1,28 @@
-{ lib, fetchFromGitHub, rustPlatform }:
+with import <nixos-unstable> { }; # bring all of Nixpkgs into scope
+# { lib, fetchFromGitHub, rustPlatform }:
 
 rustPlatform.buildRustPackage rec {
   pname = "power-panel";
-  version = "0.1.0";
+  version = "v0.1.0";
 
   src = fetchFromGitHub {
     owner = "iancleary";
     repo = pname;
     rev = version;
-    hash = lib.fakeSha256;
+    hash = "sha256-PJgFqFwl42c1W5oKYcacDMCcER/uHj7ucjzt4opvpfQ=";
   };
 
-  cargoHash = lib.fakeHash;
+  # https://nixos.wiki/wiki/Packaging/Quirks_and_Caveats
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+  ];
+
+  buildInputs = with pkgs; [
+    wayland
+    gtk4
+  ];
+
+  cargoHash = "sha256-aReLKWj3eCpQ4ct4gF+DfU37llrqyVqKd9R2OJ/3p7g=";
 
   meta = with lib; {
     description = "Rust GUI app to restart and shutdown a system";
