074fd071c26d6b6508ce28f524979286df79a8db
iancleary <github@iancleary.me>
2023-09-24 15:24:50 -0700

    Attempt to get new hash

--- modules/desktop/hyprland/power-panel.nix
+++ modules/desktop/hyprland/power-panel.nix
@@ -16,13 +16,13 @@ in
     let
       power-panel = with pkgs-unstable; rustPlatform.buildRustPackage rec {
         pname = "power-panel";
-        version = "v0.1.0";
+        version = "v1.0.0";
 
         src = fetchFromGitHub {
           owner = "iancleary";
           repo = pname;
           rev = version;
-          hash = "sha256-PJgFqFwl42c1W5oKYcacDMCcER/uHj7ucjzt4opvpfQ=";
+          hash = "sha256-XOSOaL7j93xrX/IhZzhpP5NR7Q9MyG2BSFGpsMwe82Q=";
         };
 
         # https://nixos.wiki/wiki/Packaging/Quirks_and_Caveats
