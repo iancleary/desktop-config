dc1aae0c8a8a6d3ca0d81c795b40e3a539e84a3a
Ian Cleary <github@iancleary.me>
2023-09-24 15:28:34 -0700

    power-panel v1.0.0 installation (#93)

--- modules/desktop/hyprland/power-panel.nix
+++ modules/desktop/hyprland/power-panel.nix
@@ -8,7 +8,6 @@ let
   # https://nixos.wiki/index.php?title=FAQ&oldid=3528#How_can_I_install_a_package_from_unstable_while_remaining_on_the_stable_channel.3F
   pkgs-unstable = (import inputs.nixpkgs-unstable) {
     system = "x86_64-linux";
-    config.allowUnfree = true;
   };
 in
 {
@@ -17,13 +16,13 @@ in
     let
       power-panel = with pkgs-unstable; rustPlatform.buildRustPackage rec {
         pname = "power-panel";
-        version = "v0.1.0";
+        version = "v1.0.0";
 
         src = fetchFromGitHub {
           owner = "iancleary";
           repo = pname;
           rev = version;
-          hash = "sha256-PJgFqFwl42c1W5oKYcacDMCcER/uHj7ucjzt4opvpfQ=";
+          hash = "sha256-DKjbxdvtmxSuy2I0/N2Ed0xAnLa1d5rn/XjCGPV3UHE=";
         };
 
         # https://nixos.wiki/wiki/Packaging/Quirks_and_Caveats
@@ -36,7 +35,7 @@ in
           gtk4
         ];
 
-        cargoHash = "sha256-aReLKWj3eCpQ4ct4gF+DfU37llrqyVqKd9R2OJ/3p7g=";
+        cargoHash = "sha256-XOSOaL7j93xrX/IhZzhpP5NR7Q9MyG2BSFGpsMwe82Q=";
 
         meta = with lib; {
           description = "Rust GUI app to restart and shutdown a system";
