6fca6de93d210e2f61d2713eb470ddc8ff081381
iancleary <github@iancleary.me>
2023-09-26 12:03:41 -0700

    Update flatpak script

---
+++ flatpak/setup.sh
@@ -0,0 +1,34 @@
+flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+applications=(
+  # Day to Day
+  "org.standardnotes.standardnotes"
+  "com.spotify.Client"
+  "org.mozilla.firefox"
+  "com.rafaelmardojai.Blanket"
+  "re.sonny.Junction"
+
+  # Tools
+  "com.github.tchx84.Flatseal"
+  "org.gabmus.whatip"
+
+  "io.github.seadve.Kooha"
+  "com.github.junrrein.PDFSlicer"
+  "org.kde.okular"
+  "org.libreoffice.LibreOffice"
+  "app/org.videolan.VLC/x86_64/stable"
+  "us.zoom.Zoom"
+)
+
+gnome-desktop-applications=(
+  "nl.hjdskes.gcolor3"
+  "com.mattjakeman.ExtensionManager"
+)
+
+experimental-applications=(
+  "dev.lapce.lapce"
+)
+
+for application in ${applications[@]}; do
+  flatpak install -y --user flathub $application
+done
