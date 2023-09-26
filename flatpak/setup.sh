646feecb05c30223bc1a737e3b955094fa47800c
iancleary <github@iancleary.me>
2023-09-26 12:20:54 -0700

    Add comments to flatpaks, add GNOME workbench

--- flatpak/setup.sh
+++ flatpak/setup.sh
@@ -2,31 +2,33 @@ flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/fl
 
 applications=(
   # Day to Day
-  "org.standardnotes.standardnotes"
-  "com.spotify.Client"
-  "org.mozilla.firefox"
-  "com.rafaelmardojai.Blanket"
-  "re.sonny.Junction"
+  "org.standardnotes.standardnotes" # Notes
+  "com.spotify.Client" # Music
+  "org.mozilla.firefox" # Web browser
+  "com.rafaelmardojai.Blanket" # Background noise
+  "re.sonny.Junction" # Web browser chooser (select it as a default browser)
 
   # Tools
-  "com.github.tchx84.Flatseal"
-  "org.gabmus.whatip"
+  "com.github.tchx84.Flatseal" # Manage Flatpak permissions
+  "org.gabmus.whatip" # IP address
+  "re.sonny.Workbench" # Learn and prototype with GNOME technologies
 
-  "io.github.seadve.Kooha"
-  "com.github.junrrein.PDFSlicer"
-  "org.kde.okular"
-  "org.libreoffice.LibreOffice"
-  "app/org.videolan.VLC/x86_64/stable"
-  "us.zoom.Zoom"
+  # Productivity
+  "io.github.seadve.Kooha" # Screen recording
+  "com.github.junrrein.PDFSlicer" # PDF splitter
+  "org.kde.okular" # PDF reader/editor
+  "org.libreoffice.LibreOffice" # Office suite
+  "app/org.videolan.VLC/x86_64/stable" # Video player
+  "us.zoom.Zoom" # Video conferencing
 )
 
 gnomeApplications=(
-  "nl.hjdskes.gcolor3"
-  "com.mattjakeman.ExtensionManager"
+  "nl.hjdskes.gcolor3" # Color picker
+  "com.mattjakeman.ExtensionManager" # GNOME Shell extensions
 )
 
 experimentalApplications=(
-  "dev.lapce.lapce"
+  "dev.lapce.lapce" # Code editor, written in Rust
 )
 
 for application in ${applications[@]}; do
