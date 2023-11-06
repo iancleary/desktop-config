6078123ab10d35eb5559e61e6d620b1685626709
iancleary <github@iancleary.me>
2023-11-06 09:04:06 -0700

    Format unintended uncomment

--- home/iancleary-server.nix
+++ home/iancleary-server.nix
@@ -1,22 +1,21 @@
-#
-Edit this configuration file to define what should be installed on
-  # your system.  Help is available in the configuration.nix(5) man page
-  # and in the NixOS manual (accessible by running ‘nixos-help’).
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
-  { config, inputs, pkgs, ... }: {
+{ config, inputs, pkgs, ... }: {
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+  home-manager.users.iancleary = {
+    home.stateVersion = "23.05";
     imports = [
-      inputs.home-manager.nixosModules.home-manager
+      ./common.nix
     ];
-    home-manager.users.iancleary = {
-      home.stateVersion = "23.05";
-      imports = [
-        ./common.nix
-      ];
-    };
-    # Define a user account. Don't forget to set a password with ‘passwd’.
-    users.users.iancleary = {
-      isNormalUser = true;
-      description = "iancleary";
-      extraGroups = [ "networkmanager" "wheel" "docker" "vboxsf" ];
-    };
-  }
+  };
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.iancleary = {
+    isNormalUser = true;
+    description = "iancleary";
+    extraGroups = [ "networkmanager" "wheel" "docker" "vboxsf" ];
+  };
+}
