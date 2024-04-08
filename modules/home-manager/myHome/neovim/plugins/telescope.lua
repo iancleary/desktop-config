5d451e7e5e40dfa45e7ecd7ba61f697fd7ff7394
iancleary <github@iancleary.me>
2024-04-08 08:40:40 -0700

    Attempt to show hidden files in telescope

--- modules/home-manager/myHome/neovim/plugins/telescope.lua
+++ modules/home-manager/myHome/neovim/plugins/telescope.lua
@@ -1,4 +1,24 @@
 local builtin = require("telescope.builtin")
+local setup = require("telescope.setup")
+
+setup.pickers = {
+  layout_config = {
+	width = 0.9,
+	height = 0.9,
+  },
+  find_files = {
+	hidden = true,
+  },
+  sorting_strategy = "ascending",
+  mappings = {
+	i = {
+	  ["<esc>"] = builtin.close,
+	  ["<C-j>"] = builtin.move_selection_next,
+	  ["<C-k>"] = builtin.move_selection_previous,
+	},
+  },
+}
+
 
 vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Open file picker" })
 vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Open buffer picker" })
