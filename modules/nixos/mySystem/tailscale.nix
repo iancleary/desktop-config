d07710afa535012c356dd003519746c846a51430
iancleary <github@iancleary.me>
2024-04-09 13:31:47 -0700

    Add option to use unstable pkg for tailscale

--- modules/nixos/mySystem/tailscale.nix
+++ modules/nixos/mySystem/tailscale.nix
@@ -6,13 +6,18 @@ in
 {
   options.mySystem.tailscale = {
     enable = lib.mkEnableOption "tailscale";
+    unstable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
   };
 
   config = lib.mkIf cfg.enable {
     services.tailscale.enable = true;
 
-    environment.systemPackages = with pkgs; [
-      tailscale
+    environment.systemPackages = with pkgs; mkMerge [
+      (mkIf cfg.unstable unstable.tailscale-unstable)
+      (mkIf (!cfg.unstable) tailscale)
     ];
 
     systemd.network.wait-online.ignoredInterfaces = [
