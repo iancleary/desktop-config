6f4cedae85ff1e9f48c1b388b95efacf6b86923c
Ian Cleary <github@iancleary.me>
2024-04-09 17:20:57 -0700

    Update tailscale package name in services.tailscale.package (#159)

--- modules/nixos/mySystem/tailscale.nix
+++ modules/nixos/mySystem/tailscale.nix
@@ -13,12 +13,13 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    services.tailscale.enable = true;
-
-    environment.systemPackages = with pkgs; lib.mkMerge [
-      (lib.mkIf cfg.unstable [ unstable.tailscale ])
-      (lib.mkIf (!cfg.unstable) [ tailscale ])
-    ];
+    services.tailscale = {
+      enable = true;
+      package = lib.mkMerge [
+        (lib.mkIf cfg.unstable pkgs.unstable.tailscale)
+        (lib.mkIf (!cfg.unstable) pkgs.tailscale)
+      ];
+    };
 
     systemd.network.wait-online.ignoredInterfaces = [
       "tailscale0"
