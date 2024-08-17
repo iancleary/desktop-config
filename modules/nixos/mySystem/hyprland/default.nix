673b3e5871fb8efb44c653699a326231aca294fb
iancleary <github@iancleary.me>
2024-08-16 23:21:50 -0700

    Remove exec-once of swaylock

--- modules/nixos/mySystem/hyprland/default.nix
+++ modules/nixos/mySystem/hyprland/default.nix
@@ -50,10 +50,10 @@ in
       greetd = {
         enable = true;
         settings = {
-          #initial_session = {
-          #  command = "${pkgs.hyprland}/bin/Hyprland";
-          #  user = "iancleary";
-          #};
+          initial_session = {
+            command = "${pkgs.hyprland}/bin/Hyprland";
+            user = "iancleary";
+          };
           default_session = {
             command = "${pkgs.greetd.tuigreet}/bin/tuigreet --greeting 'Welcome to NixOS!' --asterisks --remember --remember-user-session --time --cmd ${pkgs.hyprland}/bin/Hyprland";
             user = "greeter";
